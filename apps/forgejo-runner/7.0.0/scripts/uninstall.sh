d91f36fd2ddba4111bbe3fecf34dc38e20288fef
LiuShen <3162475700@qq.com>
2025-07-12 17:15:03 +0800

    😘修改为dind方式进行部署

--- apps/forgejo-runner/7.0.0/scripts/uninstall.sh
+++ apps/forgejo-runner/7.0.0/scripts/uninstall.sh
@@ -1,21 +1,21 @@
-#!/bin/bash
-set -e
+# #!/bin/bash
+# set -e
 
-CONTEXT_NAME="forgejo-runner-context"
-SOCK_PATH="/var/run/docker-forgejo-runner.sock"
+# CONTEXT_NAME="forgejo-runner-context"
+# SOCK_PATH="/var/run/docker-forgejo-runner.sock"
 
-# 删除 Docker context
-if docker context inspect "$CONTEXT_NAME" >/dev/null 2>&1; then
-  docker context rm -f "$CONTEXT_NAME"
-  echo "🗑️ 已删除 Docker context: $CONTEXT_NAME"
-else
-  echo "⚠️ Docker context $CONTEXT_NAME 不存在，跳过删除。"
-fi
+# # 删除 Docker context
+# if docker context inspect "$CONTEXT_NAME" >/dev/null 2>&1; then
+#   docker context rm -f "$CONTEXT_NAME"
+#   echo "🗑️ 已删除 Docker context: $CONTEXT_NAME"
+# else
+#   echo "⚠️ Docker context $CONTEXT_NAME 不存在，跳过删除。"
+# fi
 
-# 删除 socket 文件（如果存在）
-if [ -S "$SOCK_PATH" ]; then
-  rm -f "$SOCK_PATH"
-  echo "🗑️ 已删除 socket 文件: $SOCK_PATH"
-else
-  echo "⚠️ Socket 文件 $SOCK_PATH 不存在，跳过删除。"
-fi
+# # 删除 socket 文件（如果存在）
+# if [ -S "$SOCK_PATH" ]; then
+#   rm -f "$SOCK_PATH"
+#   echo "🗑️ 已删除 socket 文件: $SOCK_PATH"
+# else
+#   echo "⚠️ Socket 文件 $SOCK_PATH 不存在，跳过删除。"
+# fi
