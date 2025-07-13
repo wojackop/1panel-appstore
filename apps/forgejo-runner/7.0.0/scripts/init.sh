98ccd6991610738a69dadb2b26ded0b9b7feba79
LiuShen <3162475700@qq.com>
2025-07-13 09:44:10 +0800

    😎修改成root权限直接跑，不考虑部分安全问题

--- apps/forgejo-runner/7.0.0/scripts/init.sh
+++ apps/forgejo-runner/7.0.0/scripts/init.sh
@@ -16,20 +16,6 @@ chmod g+s data/.cache
 
 chmod +x ./scripts/register.sh
 
-# # 创建自定义 docker context 指向 /var/run/docker-forgejo-runner.sock
-# SOCK_PATH="/var/run/docker-forgejo-runner.sock"
-# CONTEXT_NAME="forgejo-runner-context"
-
-# # 如果 context 已存在，则先删除
-# if docker context inspect "$CONTEXT_NAME" >/dev/null 2>&1; then
-#   docker context rm -f "$CONTEXT_NAME"
-#   echo "已删除旧的 Docker context: $CONTEXT_NAME"
-# fi
-
-# docker context create "$CONTEXT_NAME" \
-#   --docker "host=unix://$SOCK_PATH"
-
 echo "✅ 初始化完成："
 echo " - 已创建 ./data 并配置权限"
 echo " - 已创建 ./scripts/register.sh 并配置权限"
-# echo " - 已创建 Docker context: $CONTEXT_NAME -> $SOCK_PATH"
